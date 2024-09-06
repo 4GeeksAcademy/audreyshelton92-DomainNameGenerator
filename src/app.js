20b48a245301a04ff9fc469fb260ce3b86cc3cab
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-09-06 16:42:24 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const generate = () => {
+  const first = ["Martha", "Freddy", "Mavis", "Marvin", "PeeWee"];
+  const middle = ["Von", "Van", "O", "La"];
+  const domain = [
+    "Izzle.com",
+    "Trump.edu",
+    "PeeWee.us",
+    "Bjorn.net",
+    "Bingbong.org"
+  ];
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < middle.length; j++) {
+      for (let k = 0; k < domain.length; k++) {
+        const name = first[i] + middle[j] + domain[k];
+        console.log(name);
+      }
+    }
+  }
+};
+generate();
